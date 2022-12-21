dd9151a866223afcfaa62cdf7427e68608393a03
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-12-21 14:57:22 +0100

    @

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Header() {
+  return (
+    <header className="App-header flex flex-row-reverse">
+      <button>Home</button>
+      <button>About Me</button>
+      <button>Projects</button>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Main() {
+  return (
+    <main className=" flex flex-row-reverse">
+      <button>Home</button>
+      <button>About Me</button>
+      <button>Projects</button>
+    </main>
+  );
+}
+
+export default Main;
