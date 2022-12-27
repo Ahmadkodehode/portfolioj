8ccf9707f6d9e21c235933f54cd8c5ae2e67b61b
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-12-27 13:28:53 +0100

    @

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Main() {
   return (
-    <main className=" flex flex-row-reverse">
+    <main className=" flex items-center justify-center h-screen">
       <h1>Hello world</h1>
     </main>
   );
