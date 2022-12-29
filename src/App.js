3f9704bf7485765a751e22939e54890062236765
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-12-29 15:04:58 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
-import Header from "./components/Header";
+
 import Footer from "./components/Footer";
+import Home from "./components/routes/Home";
+import About from "./components/routes/About";
+import Projects from "./components/routes/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <header
+        className="relative
+    w-full
+    flex flex-wrap
+    items-center
+    justify-end
+    focus:text-gray-700
+    shadow-lg
+    navbar navbar-expand-lg "
+      >
+        <Link to="/" className="m-4 hover:text-gray-700">
+          Home
+        </Link>
+        <Link to="/About" className="m-4 hover:text-gray-700">
+          About
+        </Link>
+        <Link to="/Projects" className="m-4 hover:text-gray-700">
+          Projects
+        </Link>
+      </header>
       <main className=" flex items-center justify-center h-screen">
-        <h1>Hello world</h1>
+        <Routes>
+          <Route path="/About" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="Projects" element={<Projects />} />
+        </Routes>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
