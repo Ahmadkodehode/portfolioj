71c657a47f9ed66d0fca8f4b35ae5736645e1928
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2023-01-03 13:03:47 +0100

    context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, createContext, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
@@ -8,11 +9,16 @@ import Home from "./components/routes/Home";
 import About from "./components/routes/About";
 import Projects from "./components/routes/Projects";
 
+const userTheme = createContext();
+
 function App() {
+  const [theme, setTheme] = useState("white");
   return (
-    <Router>
-      <header
-        className="relative
+    <userTheme.Provider value={theme}>
+      <Router>
+        <header
+          style={{ color: { theme } }}
+          className="relative
     w-full
     flex flex-wrap
     items-center
@@ -20,26 +26,27 @@ function App() {
     focus:text-gray-700
     shadow-lg
     navbar navbar-expand-lg "
-      >
-        <Link to="/" className="m-4 hover:text-gray-700">
-          Home
-        </Link>
-        <Link to="/About" className="m-4 hover:text-gray-700">
-          About
-        </Link>
-        <Link to="/Projects" className="m-4 hover:text-gray-700">
-          Projects
-        </Link>
-      </header>
-      <main className=" flex items-center justify-center h-screen">
-        <Routes>
-          <Route path="/About" element={<About />} />
-          <Route path="/" element={<Home />} />
-          <Route path="Projects" element={<Projects />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+        >
+          <Link to="/" className="m-4 hover:text-gray-700">
+            Home
+          </Link>
+          <Link to="/About" className="m-4 hover:text-gray-700">
+            About
+          </Link>
+          <Link to="/Projects" className="m-4 hover:text-gray-700">
+            Projects
+          </Link>
+        </header>
+        <main className=" flex items-center justify-center h-screen border-solid border-2">
+          <Routes>
+            <Route path="/About" element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="Projects" element={<Projects />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </userTheme.Provider>
   );
 }
 
